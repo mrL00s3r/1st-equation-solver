0ece6838a2a29769a8a2ddd8f2b5b32f3fedb9a4
mrL00s3r <49258602+mrL00s3r@users.noreply.github.com>
2019-06-20 00:01:42 -0300

    Update sysLim.py

--- sysLim.py
+++ sysLim.py
@@ -1,23 +1,29 @@
 #!/bin/bash/env python
 #   program to calculate linear systems
-#   CRAMER
 #   platos 06/04/19
 
-t = input("Write the number of therms:\n")
-token = [2]
-token = t.split("x")
-matrix = [[int(token[0])],[int(token[1])]]
+term = input("Write the number of terms: \n")
+unk = input("Write the number of unknowns: \n")
+indepentent = input("Write the independent terms devided by ',': \n")
+t = int(term)
+u = int(unk)
+token = [t * u] #determine the size of 'token'
+token = indepentent.split(",")
+m = [t],[u]
+if len(token) == len(m):
+    try:    #xgh
+        for i in range(0, t):
+            for j in range(0, u):
+                m = list(input())
 
-print("the number of unknowns is: "+token[1])
-print("the number of lines is: "+token[0])
-print("Write the numbers: \n")
-for j in range(0, int(token[0])):
-    b = input()
+        for i in range(0, t):
+            for j in range(0, u):
+                print(list(m))
+    except:
+        print("FUDEU")
+else:
+    print("ERROR")
 
+# TODO: make functions to determine if the system 
+# impossible, possible, ideterminant.
 
-print("Write the independent terms")
-for i in range(0, int(token[0])):
-    a = input()
-# 0 = unsolved, 1 = solved.
-# TODO: make functions to determine if the system is impossible, possible, ideterminant. '0'
-#
